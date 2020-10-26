b72f7106f05f3920c669845987477b5266a77b9d
graced2739 <73482933+graced2739@users.noreply.github.com>
2020-10-26 09:33:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Grace Dong"
+let userName = "graced2739";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
